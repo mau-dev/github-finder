89e92497733db01a353b7008d269a9c0be3d040c
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-04-28 17:37:06 +0800

    moved the ddestructured props as arguments in the arrow funcs

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const UserItem = (props) => {
+const UserItem = ({user: {id, login, avatar_url, html_url}}) => {
 	//in functional component getting rid of "this",an passing "props" as an argument in the arrow func
 	// changing this.props.user with props.user
 
-	const {id, login, avatar_url, html_url} = props.user;
+	// the comentet out destructuring moved to the arrows func arguments above
+	// const {id, login, avatar_url, html_url} = props.user;
 	return (
 		<div className='card text-center'>
 			<img src={avatar_url} alt='' className='round-img' style={{width: '60px'}} />
