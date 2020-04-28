365f2afdc360b160beb1f3fc26f61e6295ce3087
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-04-28 17:31:36 +0800

    refractoring stateless class components fo functional components with arrow func

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,15 +1,25 @@
-import React, {Component} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export class Navbar extends Component {
-	render() {
-		return (
-			<nav className='navbar bg-primary'>
-				<h1>
-					<i className={this.props.icon} /> {this.props.title}
-				</h1>
-			</nav>
-		);
-	}
-}
+const Navbar = (props) => {
+	return (
+		<nav className='navbar bg-primary'>
+			<h1>
+				<i className={props.icon} /> {props.title}
+			</h1>
+		</nav>
+	);
+};
+
+//moving the default props at the bottom, the name of the class apending with .
+Navbar.defaultProps = {
+	title: 'Github Finder',
+	icon: 'fab fa-github'
+};
+
+Navbar.propTypes = {
+	title: PropTypes.string.isRequired,
+	icon: PropTypes.string.isRequired
+};
 
 export default Navbar;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,21 +1,21 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-export class UserItem extends Component {
-	render() {
-		//change to pulling from local comp state to pulling from this.props
-		const {id, login, avatar_url, html_url} = this.props.user;
-		return (
-			<div className='card text-center'>
-				<img src={avatar_url} alt='' className='round-img' style={{width: '60px'}} />
-				<h1>{login}</h1>
-				<div>
-					<a href={html_url} className='btn btn-dark btn-sm my-1'>
-						More
-					</a>
-				</div>
+const UserItem = (props) => {
+	//in functional component getting rid of "this",an passing "props" as an argument in the arrow func
+	// changing this.props.user with props.user
+
+	const {id, login, avatar_url, html_url} = props.user;
+	return (
+		<div className='card text-center'>
+			<img src={avatar_url} alt='' className='round-img' style={{width: '60px'}} />
+			<h1>{login}</h1>
+			<div>
+				<a href={html_url} className='btn btn-dark btn-sm my-1'>
+					More
+				</a>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
 export default UserItem;
