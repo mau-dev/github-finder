a3eb847098e309bf75c4446478784aa7e2f93dad
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-04-29 20:32:08 +0800

    setting environment variables

--- src/App.js
+++ src/App.js
@@ -5,22 +5,23 @@ import Users from './components/users/Users';
 import axios from 'axios';
 
 class App extends React.Component {
-//put the red.data in the state
-state = {
-	users: [],
-	loading: false
-}
+	//put the red.data in the state
+	state = {
+		users: [],
+		loading: false
+	};
 
-
-   //make a request to the github api
+	//make a request to the github api
 	async componentDidMount() {
 		this.setState({loading: true});
 
-		const res = await axios.get('https://api.github.com/users');
-		
-		//reset the state to res.data
-		this.setState({users: res.data, loading: false})
+		const res = await axios.get(
+			`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process
+				.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
 
+		//reset the state to res.data
+		this.setState({users: res.data, loading: false});
 	}
 
 	render() {
@@ -30,7 +31,7 @@ state = {
 
 				<div className='container'>
 					{/* //passing the state with props */}
-					<Users loading={this.state.loading} users = {this.state.users}/>
+					<Users loading={this.state.loading} users={this.state.users} />
 				</div>
 			</div>
 		);
