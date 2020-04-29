df3a503934ee808a0c78982a1a43a16384210df6
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-04-29 19:29:36 +0800

    adding the spinner component, refractoring

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner'
+import PropTypes from 'prop-types';
 
-class Users extends Component {
+//destructure props, instead of props {users, loading}
+const Users = ({users, loading}) => {
 
-	render() {
-		return <div style={userStyle}>{this.props.users.map((user) => <UserItem key={user.id} user={user} />)}</div>;
-	}
+	    if (loading) {
+            return <Spinner />
+        } else
+		return (
+        <div style={userStyle}>{users.map((user) => <UserItem key={user.id} user={user} />)}</div>
+        )
 }
 
 const userStyle = {
@@ -13,5 +19,13 @@ const userStyle = {
 	gridTemplateColumns: 'repeat(3, 1fr)',
 	gridGap: '1rem'
 };
+
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
+}
+
+
+
 
 export default Users;
