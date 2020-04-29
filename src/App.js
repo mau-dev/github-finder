84844b96f061ee265897fa35710339d27375a9f9
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-04-29 19:14:33 +0800

    replaced hardcoded users state with get request from the api

--- src/App.js
+++ src/App.js
@@ -2,15 +2,35 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 
 class App extends React.Component {
+//put the red.data in the state
+state = {
+	users: [],
+	loading: false
+}
+
+
+   //make a request to the github api
+	async componentDidMount() {
+		this.setState({loading: true});
+
+		const res = await axios.get('https://api.github.com/users');
+		
+		//reset the state to res.data
+		this.setState({users: res.data, loading: false})
+
+	}
+
 	render() {
 		return (
 			<div className='App'>
 				<Navbar title='Github Finder' icon='fab fa-github' />
 
 				<div className='container'>
-					<Users />
+					{/* //passing the state with props */}
+					<Users loading={this.state.loading} users = {this.state.users}/>
 				</div>
 			</div>
 		);
